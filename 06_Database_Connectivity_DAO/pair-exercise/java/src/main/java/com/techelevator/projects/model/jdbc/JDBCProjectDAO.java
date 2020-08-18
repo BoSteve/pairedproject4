25e2572207969d37c47fbdabb73bcc4848851c39
Steve Boese <steve.f.boese@gmail.com>
2020-08-17 21:04:22 -0400

    projects class complete

--- 06_Database_Connectivity_DAO/pair-exercise/java/src/main/java/com/techelevator/projects/model/jdbc/JDBCProjectDAO.java
+++ 06_Database_Connectivity_DAO/pair-exercise/java/src/main/java/com/techelevator/projects/model/jdbc/JDBCProjectDAO.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import com.techelevator.projects.model.Project;
 import com.techelevator.projects.model.ProjectDAO;
@@ -20,16 +21,45 @@ public class JDBCProjectDAO implements ProjectDAO {
 	
 	@Override
 	public List<Project> getAllActiveProjects() {
-		return new ArrayList<>();
+		ArrayList<Project> projList = new ArrayList<Project>();
+		String sqlProjString = "SELECT * FROM project WHERE (from_date IS NOT NULL AND from_date < now() AND (to_date IS NULL OR to_date > now()))";
+		SqlRowSet sqlProjSet = jdbcTemplate.queryForRowSet(sqlProjString);
+		while (sqlProjSet.next()) {
+			Project proj = addRowToProjects(sqlProjSet);
+			projList.add(proj);
+		}
+		
+		return projList;
+	}
+
+	private Project addRowToProjects(SqlRowSet sqlProjSet) {
+		Project addRowProject = new Project();
+		addRowProject.setId(sqlProjSet.getLong("project_id"));
+		addRowProject.setName(sqlProjSet.getString("name"));
+		
+		
+		
+		// +++++++++++++ Check this if an error occurs. +++++++++++++++++++++++++++++
+//		addRowProject.setStartDate(sqlProjSet.getDate("from_date").toLocalDate());
+//		addRowProject.setEndDate(sqlProjSet.getDate("to_date").toLocalDate());
+
+		
+
+	
+		return addRowProject;
 	}
 
 	@Override
-	public void removeEmployeeFromProject(Long projectId, Long employeeId) {
+	public void removeEmployeeFromProject(Long employeeId, Long projectId) {
+		String removeEmp = "DELETE FROM project_employee WHERE employee_id = ? AND project_id = ?";
+		jdbcTemplate.update(removeEmp, employeeId, projectId);
 		
 	}
 
 	@Override
 	public void addEmployeeToProject(Long projectId, Long employeeId) {
+		String addEmp = "INSERT INTO project_employee (project_id, employee_id) VALUES (?, ?)";
+		jdbcTemplate.update(addEmp, projectId, employeeId);
 		
 	}
 
